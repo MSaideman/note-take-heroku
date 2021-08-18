69e4a0645db41253cbf4f2feefc3169768559af9
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-18 13:13:01 -0400

    added notes to server.js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+// require modules
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+// call express module
 const app = express();
 
+// heroku port set-up
 const PORT = process.env.PORT || 3001;
 
+// require data json file
 const notes = require('./db/db.json');
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// routes
 app.get('/api/notes', (req, res) => {
     res.json(notes.slice(1));
 });
@@ -28,6 +34,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// create notes function
 function createNewNote(body, notesArr) {
     const newNote = body;
     if (!Array.isArray(notesArr))
@@ -66,13 +73,14 @@ function deleteNote(id, notesArr) {
             break;
         }
     }
-}
 
+// delete notes function
 app.delete('/api/notes/:id',(req,res) => {
     deleteNote(req.params.id, notes)
     res.json(true);
 });
 
+// listen to port function
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
