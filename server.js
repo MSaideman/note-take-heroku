bb20e3a9cf2c53a93db0d03197245909558421df
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-18 12:39:12 -0400

    updated files to github repo

---
+++ server.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const notes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArr) {
+    const newNote = body;
+    if (!Array.isArray(notesArr))
+        notesArr = [];
+    
+    if (notesArr.length === 0)
+        notesArr.push(0);
+
+    body.id = notesArr[0];
+    notesArr[0]++;
+
+    notesArr.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArr, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArr) {
+    for (let i = 0; i < notesArr.length; i++) {
+        let note = notesArr[i];
+
+        if (note.id == id) {
+            notesArr.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArr, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id',(req,res) => {
+    deleteNote(req.params.id, notes)
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+
